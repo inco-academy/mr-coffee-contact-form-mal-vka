eee5d5cfef1855c31096e146fc5e3657afdb6302
mal-vka <malwina.g@gmial.com>
2022-02-16 18:41:27 +0100

    add simple validation for contact form

--- js/form.js
+++ js/form.js
@@ -1,22 +1,94 @@
-const lastNameElement = document.querySelector(".js-lastName");
-const firsttNameElement = document.querySelector(".js-firstName");
-const phoneNumberElement = document.querySelector(".js-phoneNumber");
-const emailAddressElement = document.querySelector(".js-emailAddress");
-const messageElement = document.querySelector(".js-message");
+{
+    const contactFormElement = document.querySelector(".js-contactForm");
+    const lastNameElement = document.querySelector(".js-lastName");
+    const firstNameElement = document.querySelector(".js-firstName");
+    const phoneNumberElement = document.querySelector(".js-phoneNumber");
+    const emailAddressElement = document.querySelector(".js-emailAddress");
+    const messageElement = document.querySelector(".js-message");
 
-// const lastName = lastNameElement.value;
-// const firsttName = firsttNameElement.value;
-// const phoneNumber = phoneNumberElement.value;
-// const emailAddress = emailAddressElement.value;
-// const message = messageElement.value;
+    const modal = document.querySelector(".js-modal");
+    const overlay = document.querySelector(".overlay");
 
-const formButton = document.querySelector(".js-formButton");
+    const closeButton = document.querySelector(".js-closeButton");
 
-formButton.addEventListener("click", (event) => {
-    event.preventDefault();
+    function GetValues(firstName, lastName, phoneNumber, emailAddress, message) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        this.message = message;
+    };
 
-    console.log(`
-    ${firsttNameElement.value} ${lastNameElement.value} napisał(a):\n
-    ${messageElement.value}\n
-    kontakt: email ${emailAddressElement.value}, tel. ${phoneNumberElement.value}`);
-});
+    const displayModal = () => {
+        modal.classList.remove("hidden");
+        overlay.classList.remove("hidden");
+    };
+
+    const closeModal = () => {
+        modal.classList.add("hidden");
+        overlay.classList.add("hidden");
+    };
+
+    const logInConsole = (obj) => {
+        console.log(`${obj.firstName} ${obj.lastName} napisał(a):\n${obj.message}\nkontakt: email ${obj.emailAddress}, tel. ${obj.phoneNumber}`);
+    };
+
+    const isFilled = (arr) => {
+        return arr.every(value => value !== "");
+    };
+
+    const isText = (str) => {
+        const textRegex = /[a-zA-Z]{2,50}/;
+        return textRegex.test(str);
+    };
+
+    const isPhonePattern = (str) => {
+        const phoneNumberRegex = /[0-9]{3}-?[0-9]{3}-?[0-9]{3}/;
+        return phoneNumberRegex.test(str);
+    };
+
+    const isEmailAddressPattern = (str) => {
+        const emailAddressRegex = /\S+@\S+\.\S+/;
+        return emailAddressRegex.test(str);
+    };
+
+    const validate = (obj) => {
+        if (!isFilled(Object.values(obj))) {
+            return false;
+        } else if (!(isText(obj.firstName) && isText(obj.lastName))) {
+            return false;
+        } else if (!isPhonePattern(obj.phoneNumber)) {
+            return false;
+        } else if (!isEmailAddressPattern(obj.emailAddress)) {
+            return false;
+        };
+        return true;
+    };
+
+    closeButton.addEventListener("click", closeModal);
+
+    overlay.addEventListener("click", closeModal);
+
+    document.addEventListener("keydown", (event) => {
+        if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+            closeModal();
+        };
+    });
+
+    contactFormElement.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const formValues = new GetValues(
+            firstNameElement.value,
+            lastNameElement.value,
+            phoneNumberElement.value,
+            emailAddressElement.value,
+            messageElement.value
+        );
+
+        if (validate(formValues)) {
+            displayModal();
+            logInConsole(formValues);
+        };
+    });
+}
